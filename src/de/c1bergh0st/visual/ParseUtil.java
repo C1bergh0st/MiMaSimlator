421ad19490e98a77309130a32113bcafb958b9f7
Philipp Berdesinski <36385593+C1bergh0st@users.noreply.github.com>
2018-11-27 00:07:43 +0100

    Added a flipton of Javadoc to ParseUtil.java

--- src/de/c1bergh0st/visual/ParseUtil.java
+++ src/de/c1bergh0st/visual/ParseUtil.java
@@ -2,10 +2,14 @@ package de.c1bergh0st.visual;
 
 import de.c1bergh0st.debug.Debug;
 
+/**
+ *  Just a Utility Class to convert from Strings to binary and vice versa
+ */
+@SuppressWarnings({"StringConcatenationInLoop", "WeakerAccess"})
 public class ParseUtil {
-    /**
-     *  Just a Utility Class to convert from Strings to binary and vice versa
-     */
+
+    //A List of possible Commands
+    public static final String[] commands = {"LDC", "LDV", "STV", "ADD", "AND", "OR", "XOR", "EQL", "JMP", "JMN", "LDIV", "STIV", "RAR", "NOT", "EMPTY", "HALT"};
 
 
     /**
@@ -18,71 +22,110 @@ public class ParseUtil {
         while(trimmedString.length() < 24){
             trimmedString = "0" + trimmedString;
         }
-        Debug.send(trimmedString);
 
-        if(trimmedString.matches("^([1,0]{24})") && trimmedString.length() == 24){
-            return true;
-        }
-        return false;
+        return trimmedString.matches("^([1,0]{24})$");
     }
 
+
+    /**
+     * @param commandString Command
+     * @return Whether the given commandString is valid
+     */
     public static boolean validCommand(String commandString){
         String s = commandString.replaceAll("\\s","");
-        if(s.matches("^(LDC|LDV|STV|ADD|AND|OR|XOR|EQL|JMP|JMN|LDIV|STIV|RAR|NOT|EMPTY|HALT)[0-9]+$")){
+        if(s.matches("^(((LDC|LDV|STV|ADD|AND|OR|XOR|EQL|JMP|JMN|LDIV|STIV)[0-9]+)|(RAR|NOT|EMPTY|HALT))$")){
             return true;
         }
         Debug.send("Regex didnt match: "+s);
         return false;
     }
 
+
+    /**
+     * @param command A Command witch should match the regex given in validCommand(String commandString)
+     * @return A 24 bit Integer Representation of a valid Command
+     */
     public static int parseCommand(String command){
-        String s = command.trim();
+        String s = command.replaceAll("\\s","");
         //We Remove characters until we only have a number left.
         while(!s.matches("^[0-9]+$") && s.length() > 1){
             s = s.substring(1);
         }
+        if(!s.matches("^[0-9]+$")){
+            s = "0";
+        }
         //we take bits 0 to 19    Nicely prevents bigger values
-        int result = maskAdr(Integer.parseInt(s));
+        int result = mask20(Integer.parseInt(s));
         //we add bits 20 to 23 to it
         result += getOpCode(command)<<20;
-        return maskValue(result);
+        return mask24(result);
     }
 
+
+    /**
+     * @param command A Command that should contain a possible Command
+     * @return An Integer with the OpCode of the given Command String => see ParseUtil.commands
+     */
+    @SuppressWarnings("ConstantConditions")
     public static int getOpCode(String command){
         int i = 0;
-        String[] commands = {"LDC", "LDV", "STV", "ADD", "AND", "OR", "XOR", "EQL", "JMP", "JMN", "LDIV", "STIV", "RAR", "NOT", "EMPTY", "HALT"};
 
-        while(!command.contains(commands[i]) && i < 16){
+        //if the Command is contained within the String we get the OPCode back
+        while(!command.contains(commands[i]) && i < commands.length){
             i++;
         }
 
+        //If i == 16 we didnt find Anything so we set the code to LDC (Code :0)
         if(i == 16){
             i = 0;
         }
+
         return i;
     }
 
+    /**
+     * @param i Integer
+     * @return A Full Binary String Representation with leading zeros.
+     */
+
     public static String toBinaryString(int i){
-        String s = Integer.toBinaryString(i);
+        //Create a String from the 24 bit Value of i
+        String s = Integer.toBinaryString(mask24(i));
+        //Pads the String with Leading zeros untill it is 24 'bits' long
         while(s.length() < 24){
             s = "0" + s;
         }
         return s;
     }
 
-    public static int maskValue(int i){
+
+    /**
+     * @param i Integer
+     * @return Returns an int with it's highest byte set to zero
+     */
+    public static int mask24(int i){
         return i & 0b00000000111111111111111111111111;
     }
 
-    public static int maskAdr(int i){
+
+    /**
+     * @param i Integer
+     * @return Returns an int with it's highest 12 bits set to zero
+     */
+    public static int mask20(int i){
+        //Return
         return i & 0b00000000000011111111111111111111;
     }
 
+
+    /**
+     * @param value Integer
+     * @return Command reprentation of the Input
+     */
     public static String code(int value){
-        int safeValue = maskValue(value);
+        int safeValue = mask24(value);
         byte b = (byte)(safeValue>>>20);
         safeValue &= 0b00000000000011111111111111111111;
-        String[] commands = {"LDC", "LDV", "STV", "ADD", "AND", "OR", "XOR", "EQL", "JMP", "JMN", "LDIV", "STIV", "RAR", "NOT", "EMPTY", "HALT"};
         if(b >= 0 && b < 16){
             String s =  commands[b] +" ";
             if(b < 12){
