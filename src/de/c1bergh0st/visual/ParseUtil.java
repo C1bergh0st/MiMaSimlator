f7941104a4c9aad66a1846fe8e93a4bdd9ba10ce
Philipp Berdesinski <36385593+C1bergh0st@users.noreply.github.com>
2018-11-26 23:19:34 +0100

    Command Parsing

--- src/de/c1bergh0st/visual/ParseUtil.java
+++ src/de/c1bergh0st/visual/ParseUtil.java
@@ -1,5 +1,7 @@
 package de.c1bergh0st.visual;
 
+import de.c1bergh0st.debug.Debug;
+
 public class ParseUtil {
     /**
      *  Just a Utility Class to convert from Strings to binary and vice versa
@@ -13,14 +15,76 @@ public class ParseUtil {
      */
     public static boolean validBinary(String binaryString){
         String trimmedString = binaryString.trim();
-        while(trimmedString.length() < 25){
-            trimmedString = "0"+trimmedString;
+        while(trimmedString.length() < 24){
+            trimmedString = "0" + trimmedString;
         }
+        Debug.send(trimmedString);
+
         if(trimmedString.matches("^([1,0]{24})") && trimmedString.length() == 24){
             return true;
         }
         return false;
     }
 
+    public static boolean validCommand(String commandString){
+        String s = commandString.trim();
+        if(s.matches("^(LDC|LDV|STV|ADD|AND|OR|XOR|EQL|JMP|JMN|LDIV|STIV|RAR|NOT|EMPTY|HALT)[0-9]+$")){
+            return true;
+        }
+        return false;
+    }
 
+    public static int parseCommand(String command){
+        String s = command.trim();
+        //We Remove characters until we only have a number left.
+        while(!s.matches("^[0-9]+$") && s.length() > 1){
+            s = s.substring(1);
+        }
+        //we take bits 0 to 19    Nicely prevents bigger values
+        int result = maskAdr(Integer.parseInt(s));
+        //we add bits 20 to 23 to it
+        result += getOpCode(command)<<20;
+        return maskValue(result);
+    }
+
+    public static int getOpCode(String command){
+        int i = 0;
+        String[] commands = {"LDC", "LDV", "STV", "ADD", "AND", "OR", "XOR", "EQL", "JMP", "JMN", "LDIV", "STIV", "RAR", "NOT", "EMPTY", "HALT"};
+
+        while(!command.contains(commands[i]) && i < 16){
+            i++;
+        }
+
+        if(i == 16){
+            i = 0;
+        }
+        return i;
+    }
+
+    public static String toBinaryString(int i){
+        String s = Integer.toBinaryString(i);
+        while(s.length() < 24){
+            s = "0" + s;
+        }
+        return s;
+    }
+
+    public static int maskValue(int i){
+        return i & 0b00000000111111111111111111111111;
+    }
+
+    public static int maskAdr(int i){
+        return i & 0b00000000000011111111111111111111;
+    }
+
+    public static String code(int value){
+        int safeValue = maskValue(value);
+        byte b = (byte)(safeValue>>>20);
+        safeValue &= 0b00000000000011111111111111111111;
+        String[] commands = {"LDC", "LDV", "STV", "ADD", "AND", "OR", "XOR", "EQL", "JMP", "JMN", "LDIV", "STIV", "RAR", "NOT", "EMPTY", "HALT"};
+        if(b >= 0 && b < 16){
+            return commands[b] + safeValue;
+        }
+        return "Error";
+    }
 }
--- src/de/c1bergh0st/visual/TableListener.java
+++ src/de/c1bergh0st/visual/TableListener.java
@@ -11,41 +11,73 @@ import javax.swing.event.TableModelListener;
 class TableListener implements TableModelListener {
     JTable table;
     Speicher speicher;
+    long lastchange;
 
     public TableListener(JTable table, Speicher speicher) {
         this.table = table;
         this.speicher = speicher;
+        lastchange = System.currentTimeMillis();
     }
 
     public void tableChanged(TableModelEvent e) {
         int adress = e.getFirstRow();
         int inputField = e.getColumn();
-
+        String value = (String)table.getModel().getValueAt(adress, inputField);
         if(e.getType() == TableModelEvent.UPDATE){
+            if(lastchange > System.currentTimeMillis() - 100){
+                return;
+            }
+            lastchange = System.currentTimeMillis();
 
-            Debug.send("Row:"+ adress + " Column:" + inputField + " to: "+ table.getModel().getValueAt(adress, inputField));
+            Debug.send("Row:"+ adress + " Column:" + inputField + " to: "+ value);
 
             if(isValidInput(adress, inputField)){
                 speicher.setMem(adress,parseValue(adress, inputField));
+                //proper Formatting
+                updateBinary(adress);
+                updateRepr(adress);
             }else{
                 //If the new Value is invalid we reset it
-                table.getModel().setValueAt(Integer.toBinaryString(speicher.getMem(adress)),adress,inputField);
+                updateBinary(adress);
+                updateRepr(adress);
             }
         }
 
     }
 
+    private void updateBinary(int adress) {
+        table.getModel().setValueAt(ParseUtil.toBinaryString(speicher.getMem(adress)),adress,1);
+    }
+
+    private void updateRepr(int adress) {
+        int value = speicher.getMem(adress);
+        table.getModel().setValueAt(ParseUtil.code(value),adress,2);
+    }
+
     private int parseValue(int adress, int inputField) {
+        String value = (String)table.getModel().getValueAt(adress, inputField);
+        if(inputField == 1){
+            return Integer.parseInt(value.trim(), 2);
+        } else if (inputField == 2){
+            return ParseUtil.parseCommand(value);
+        }
         return 0;
     }
 
     private boolean isValidInput(int adress, int inputField) {
         String value = (String)table.getModel().getValueAt(adress, inputField);
+        //check if the newly saved value lies within the adress space
         if(adress >= 0 && adress <= Steuerwerk.MAX_ADRESS){
-            if(inputField == 1) {
-                if (ParseUtil.validBinary(value)){
 
+            if(inputField == 1) {//Binary Input
+                if (ParseUtil.validBinary(value)){
+                    return true;
                 }
+            } else if(inputField == 2){//Command style input
+                if(ParseUtil.validCommand(value)){
+                    return true;
+                }
+
             }
         }
         return false;
